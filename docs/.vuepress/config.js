5116ee3856657100a9e1e4261d5c9d44e84a460e
Valentyn13 <valentyn13git@gmail.com>
2023-01-07 11:46:52 +0200

    fix

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -86,7 +86,7 @@ module.exports = {
 
     lastUpdated: "Останнє оновлення", // string | boolean
     // Assumes GitHub. Can also be a full GitLab url.
-    repo: "https://github.com/Valentyn13/DB_LAB_6",
+    repo: "https://github.com/PerveevEvgen/DB_LAB_6",
     // Customising the header label
     // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
     repoLabel: "Github",
